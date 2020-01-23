215d1c5c90e587b4b8f5ecc192c378412595000f
atirudom <atirach.int@gmail.com>
2020-01-24 01:05:44 +1100

    Add ATM functionality and work with Login session.

---
+++ Controllers/ATMController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Assignment2.Data;
+using Assignment2.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Assignment2.Controllers
+{
+    public class ATMController : Controller
+    {
+        private readonly MainContext _context;
+        private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID)).Value;
+
+        public ATMController(MainContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Customer
+        public async Task<IActionResult> Index()
+        {
+            var customer = await _context.Customers.FindAsync(CustomerID);
+
+            return View(customer);
+        }
+    }
+}
