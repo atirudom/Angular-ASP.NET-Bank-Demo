56630f56e9b00e2460d0cd2382c645c633c36596
atirudom <atirach.int@gmail.com>
2020-01-24 01:04:52 +1100

    develop seedData

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -17,11 +17,14 @@ namespace Assignment2.Data
             if (context.Customers.Any())
                 return; // DB has already been seeded.
 
+            const string dateFormat = "dd/MM/yyyy hh:mm:ss tt";
+            DateTime now = DateTime.Now;
+
             context.Customers.AddRange(
                 new Customer
                 {
                     CustomerID = 2100,
-                    CustomerName = "Matthew Bolger",
+                    Name = "Matthew Bolger",
                     Address = "123 Fake Street",
                     City = "Melbourne",
                     PostCode = "3000",
@@ -30,7 +33,7 @@ namespace Assignment2.Data
                 new Customer
                 {
                     CustomerID = 2200,
-                    CustomerName = "Rodney Cocker",
+                    Name = "Rodney Cocker",
                     Address = "456 Real Road",
                     City = "Melbourne",
                     PostCode = "3005",
@@ -39,7 +42,7 @@ namespace Assignment2.Data
                 new Customer
                 {
                     CustomerID = 2300,
-                    CustomerName = "Shekhar Kalra",
+                    Name = "Shekhar Kalra",
                     Phone = "0351548454"
                 });
 
@@ -48,86 +51,100 @@ namespace Assignment2.Data
                 {
                     UserID = "12345678",
                     CustomerID = 2100,
-                    PasswordHash = "YBNbEL4Lk8yMEWxiKkGBeoILHTU7WZ9n8jJSy8TNx0DAzNEFVsIVNRktiQV+I8d2"
+                    PasswordHash = "YBNbEL4Lk8yMEWxiKkGBeoILHTU7WZ9n8jJSy8TNx0DAzNEFVsIVNRktiQV+I8d2",
+                    ModifyDate = now
                 },
                 new Login
                 {
                     UserID = "38074569",
                     CustomerID = 2200,
-                    PasswordHash = "EehwB3qMkWImf/fQPlhcka6pBMZBLlPWyiDW6NLkAh4ZFu2KNDQKONxElNsg7V04"
+                    PasswordHash = "EehwB3qMkWImf/fQPlhcka6pBMZBLlPWyiDW6NLkAh4ZFu2KNDQKONxElNsg7V04",
+                    ModifyDate = now
                 },
                 new Login
                 {
                     UserID = "17963428",
                     CustomerID = 2300,
-                    PasswordHash = "LuiVJWbY4A3y1SilhMU5P00K54cGEvClx5Y+xWHq7VpyIUe5fe7m+WeI0iwid7GE"
+                    PasswordHash = "LuiVJWbY4A3y1SilhMU5P00K54cGEvClx5Y+xWHq7VpyIUe5fe7m+WeI0iwid7GE",
+                    ModifyDate = now
                 });
 
             context.Accounts.AddRange(
                 new Account
                 {
-                    AccountNumber = 4100,
                     AccountType = AccountType.Saving,
                     CustomerID = 2100,
-                    Balance = 100
+                    Balance = 100,
+                    ModifyDate = now
                 },
                 new Account
                 {
-                    AccountNumber = 4101,
                     AccountType = AccountType.Checking,
                     CustomerID = 2100,
-                    Balance = 500
+                    Balance = 500,
+                    ModifyDate = now
                 },
                 new Account
                 {
-                    AccountNumber = 4200,
                     AccountType = AccountType.Saving,
                     CustomerID = 2200,
-                    Balance = 500.95m
+                    Balance = 500.95m,
+                    ModifyDate = now
                 },
                 new Account
                 {
-                    AccountNumber = 4300,
                     AccountType = AccountType.Checking,
                     CustomerID = 2300,
-                    Balance = 1250.50m
+                    Balance = 1250.50m,
+                    ModifyDate = now
                 });
 
-            const string openingBalance = "Opening balance";
-            const string format = "dd/MM/yyyy hh:mm:ss tt";
-            context.Transactions.AddRange(
-                new Transaction
+            //const string openingBalance = "Opening balance";
+            //context.Transactions.AddRange(
+            //    new Transaction
+            //    {
+            //        TransactionType = TransactionType.Deposit,
+            //        AccountNumber = 4100,
+            //        Amount = 100,
+            //        Comment = openingBalance,
+            //        TransactionTimeUtc = DateTime.ParseExact("19/12/2019 08:00:00 PM", dateFormat, null)
+            //    },
+            //    new Transaction
+            //    {
+            //        TransactionType = TransactionType.Deposit,
+            //        AccountNumber = 4101,
+            //        Amount = 500,
+            //        Comment = openingBalance,
+            //        TransactionTimeUtc = DateTime.ParseExact("19/12/2019 08:30:00 PM", dateFormat, null)
+            //    },
+            //    new Transaction
+            //    {
+            //        TransactionType = TransactionType.Deposit,
+            //        AccountNumber = 4200,
+            //        Amount = 500.95m,
+            //        Comment = openingBalance,
+            //        TransactionTimeUtc = DateTime.ParseExact("19/12/2019 09:00:00 PM", dateFormat, null)
+            //    },
+            //    new Transaction
+            //    {
+            //        TransactionType = TransactionType.Deposit,
+            //        AccountNumber = 4300,
+            //        Amount = 1250.50m,
+            //        Comment = "Opening balance",
+            //        TransactionTimeUtc = DateTime.ParseExact("19/12/2019 10:00:00 PM", dateFormat, null)
+            //    });
+
+            context.Payees.AddRange(
+                new Payee
                 {
-                    TransactionType = TransactionType.Deposit,
-                    AccountNumber = 4100,
-                    Amount = 100,
-                    Comment = openingBalance,
-                    TransactionTimeUtc = DateTime.ParseExact("19/12/2019 08:00:00 PM", format, null)
-                },
-                new Transaction
-                {
-                    TransactionType = TransactionType.Deposit,
-                    AccountNumber = 4101,
-                    Amount = 500,
-                    Comment = openingBalance,
-                    TransactionTimeUtc = DateTime.ParseExact("19/12/2019 08:30:00 PM", format, null)
-                },
-                new Transaction
-                {
-                    TransactionType = TransactionType.Deposit,
-                    AccountNumber = 4200,
-                    Amount = 500.95m,
-                    Comment = openingBalance,
-                    TransactionTimeUtc = DateTime.ParseExact("19/12/2019 09:00:00 PM", format, null)
-                },
-                new Transaction
-                {
-                    TransactionType = TransactionType.Deposit,
-                    AccountNumber = 4300,
-                    Amount = 1250.50m,
-                    Comment = "Opening balance",
-                    TransactionTimeUtc = DateTime.ParseExact("19/12/2019 10:00:00 PM", format, null)
-                });
+                    PayeeName = "Atirudom",
+                    Address = "400 King Street",
+                    City = "Melbourne",
+                    State = "VIC",
+                    PostCode = "3020",
+                    Phone = "(61) - 29374612"
+                }
+            );
 
             context.SaveChanges();
         }
