9b31f4d9407da1db5cc67edbc3c9fa49e571b30c
atirudom <atirach.int@gmail.com>
2020-01-24 01:04:29 +1100

    add sql for reseting DB value

---
+++ Data/Sql/deleteAllinDB.sql
@@ -0,0 +1,7 @@
+delete from [dbo].[Transactions]
+delete from [dbo].[Payees]
+delete from [dbo].[Accounts]
+delete from [dbo].[BillPays]
+delete from [dbo].[Logins]
+delete from [dbo].[Customers]
+GO
