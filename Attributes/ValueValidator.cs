c4960d325dd6e7aa9bc824b11db8f46becda397c
atirudom <atirach.int@gmail.com>
2020-01-25 20:34:33 +1100

    Add value validating feature

---
+++ Attributes/ValueValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Attributes
+{
+    public static class ValueValidator
+    {
+        public static bool HasMoreThanNDecimalPlaces(this decimal value, int n) => decimal.Round(value, n) != value;
+        public static bool HasMoreThanTwoDecimalPlaces(this decimal value) => value.HasMoreThanNDecimalPlaces(2);
+    }
+}
