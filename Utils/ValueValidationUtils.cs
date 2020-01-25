a953ec0c358a9024cebe5268b4e13ce987b2cf54
atirudom <atirach.int@gmail.com>
2020-01-25 20:53:07 +1100

    Move value validator to Utils

--- Utils/ValueValidationUtils.cs
+++ Utils/ValueValidationUtils.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Assignment2.Attributes
+namespace Assignment2.Utils
 {
-    public static class ValueValidator
+    public static class ValueValidationUtils
     {
         public static bool HasMoreThanNDecimalPlaces(this decimal value, int n) => decimal.Round(value, n) != value;
         public static bool HasMoreThanTwoDecimalPlaces(this decimal value) => value.HasMoreThanNDecimalPlaces(2);
