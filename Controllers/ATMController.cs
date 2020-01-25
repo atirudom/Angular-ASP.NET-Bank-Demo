ecfad0372fedcc3bba198562e8f8ddaea40b448e
atirudom <atirach.int@gmail.com>
2020-01-25 20:54:30 +1100

    develop ATM withdraw and deposit features

--- Controllers/ATMController.cs
+++ Controllers/ATMController.cs
@@ -9,6 +9,9 @@ using Assignment2.Data;
 using Assignment2.Models;
 using Microsoft.AspNetCore.Http;
 using Assignment2.Attributes;
+using Assignment2.Models.Adapter;
+using Assignment2.CustomExceptions;
+using Assignment2.Utils;
 
 namespace Assignment2.Controllers
 {
@@ -35,10 +38,10 @@ namespace Assignment2.Controllers
         [HttpPost]
         public async Task<IActionResult> Deposit(int id, decimal amount)
         {
-            var account = await _context.Accounts.FindAsync(id);
+            Account account = await _context.Accounts.FindAsync(id);
 
-            if (amount <= 0)
-                ModelState.AddModelError(nameof(amount), "Amount must be positive.");
+            if (amount < 0)
+                ModelState.AddModelError(nameof(amount), "Amount must be at least 1.");
             if (amount.HasMoreThanTwoDecimalPlaces())
                 ModelState.AddModelError(nameof(amount), "Amount cannot have more than 2 decimal places.");
             if (!ModelState.IsValid)
@@ -47,19 +50,46 @@ namespace Assignment2.Controllers
                 return View(account);
             }
 
-            // Note this code could be moved out of the controller, e.g., into the Model.
-            account.Balance += amount;
-            account.Transactions.Add(
-                new Transaction
-                {
-                    TransactionType = TransactionType.Deposit,
-                    Amount = amount,
-                    TransactionTimeUtc = DateTime.UtcNow
-                });
+            try
+            {
+                AccountAdapter accountAdapter = new AccountAdapter(account);
+                accountAdapter.Deposit(amount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (BusinessRulesException e)
+            {
+                ModelState.AddModelError(nameof(amount), e.errMsg);
+                return View(account);
+            }
+        }
 
-            await _context.SaveChangesAsync();
+        internal async Task<IActionResult> Withdraw(int id, decimal amount)
+        {
+            Account account = await _context.Accounts.FindAsync(id);
 
-            return RedirectToAction(nameof(Index));
+            if (amount < 0)
+                ModelState.AddModelError(nameof(amount), "Amount must be at least 1.");
+            if (amount.HasMoreThanTwoDecimalPlaces())
+                ModelState.AddModelError(nameof(amount), "Amount cannot have more than 2 decimal places.");
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Amount = amount;
+                return View(account);
+            }
+
+            try
+            {
+                AccountAdapter accountAdapter = new AccountAdapter(account);
+                accountAdapter.Withdraw(amount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (BusinessRulesException e)
+            {
+                ModelState.AddModelError(nameof(amount), e.errMsg);
+                return View(account);
+            }
         }
     }
 }
--- Models/Adapter/AccountAdapter.cs
+++ Models/Adapter/AccountAdapter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assignment2.CustomExceptions;
+using Assignment2.Models.Factory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,64 @@ namespace Assignment2.Models.Adapter
 {
     public class AccountAdapter
     {
+        private decimal WithdrawServiceFee = 0.1m;
+        private Account Account;
+
+        // Set account on creating with constructor
+        public AccountAdapter(Account account)
+        {
+            this.Account = account;
+        }
+
+        internal void Deposit(decimal amount)
+        {
+            if (amount <= 0) throw new BusinessRulesException("Amount cannot be lower than 1!");
+            Account.ChangeBalance(amount);
+            Transaction transaction = TransactionFactory.GenerateTransaction(Account.AccountNumber, TransactionType.Deposit, amount);
+            Account.Transactions.Add(transaction);
+            Console.WriteLine("\nGenerated Transaction: \n{0}", transaction);
+        }
+
+        internal void Withdraw(decimal amount)
+        {
+            if (amount <= 0) throw new BusinessRulesException("Amount cannot be lower than 0!");
+
+            // get incurred fee and add to total amount to reduce
+            decimal totalAmountToReduce = 0;
+            decimal serviceFee = GetIncurredFee();
+            totalAmountToReduce += amount;
+            totalAmountToReduce += serviceFee;
+
+            // Perform changing balance first to check if satisfying the transferring conditions
+            Account.ChangeBalance(-totalAmountToReduce);
+
+            // Generate transaction
+            Transaction transaction = TransactionFactory.GenerateTransaction(Account.AccountNumber, TransactionType.Withdraw, amount);
+            Account.Transactions.Add(transaction);
+            Console.WriteLine("\nGenerated Transaction: \n{0}", transaction);
+
+            // apply incurred fee and generate service fee transaction
+            if (serviceFee != 0)
+            {
+                Transaction feeTransaction = TransactionFactory.GenerateTransaction(Account.AccountNumber, TransactionType.ServiceCharge, serviceFee);
+                Account.Transactions.Add(feeTransaction);
+                Console.WriteLine("\nGenerated Transaction: \n{0}", feeTransaction);
+            }
+        }
+
+        private decimal GetIncurredFee()
+        {
+            decimal incurredFee = 0;
+            List<Transaction> accountTransactions = Account.Transactions;
+            int allowedNumOfFreeTransactions = 4;
+            foreach (Transaction t in accountTransactions)
+            {
+                if (allowedNumOfFreeTransactions <= 0) break;
+                if (t.TransactionType == TransactionType.Withdraw || t.TransactionType == TransactionType.Transfer && t.AccountNumber == Account.AccountNumber)
+                    allowedNumOfFreeTransactions -= 1;
+            }
+            if (allowedNumOfFreeTransactions <= 0) incurredFee += WithdrawServiceFee;
+            return incurredFee;
+        }
     }
 }
