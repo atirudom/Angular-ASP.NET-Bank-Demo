5733d393fd4b2d4e3c8e6067585e248440debbeb
atirudom <atirach.int@gmail.com>
2020-01-25 20:30:05 +1100

    Add deposit feature in controller

--- Controllers/ATMController.cs
+++ Controllers/ATMController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Assignment2.Data;
 using Assignment2.Models;
 using Microsoft.AspNetCore.Http;
+using Assignment2.Attributes;
 
 namespace Assignment2.Controllers
 {
@@ -27,6 +28,38 @@ namespace Assignment2.Controllers
             var customer = await _context.Customers.FindAsync(CustomerID);
 
             return View(customer);
+        }
+
+        public async Task<IActionResult> Deposit(int id) => View(await _context.Accounts.FindAsync(id));
+
+        [HttpPost]
+        public async Task<IActionResult> Deposit(int id, decimal amount)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+
+            if (amount <= 0)
+                ModelState.AddModelError(nameof(amount), "Amount must be positive.");
+            if (amount.HasMoreThanTwoDecimalPlaces())
+                ModelState.AddModelError(nameof(amount), "Amount cannot have more than 2 decimal places.");
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Amount = amount;
+                return View(account);
+            }
+
+            // Note this code could be moved out of the controller, e.g., into the Model.
+            account.Balance += amount;
+            account.Transactions.Add(
+                new Transaction
+                {
+                    TransactionType = TransactionType.Deposit,
+                    Amount = amount,
+                    TransactionTimeUtc = DateTime.UtcNow
+                });
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
