e3c270d00a68028d2953fb3b05f6efd31c953bdf
atirudom <atirach.int@gmail.com>
2020-01-26 19:02:13 +1100

    Tweak and fix login

--- Models/Login.cs
+++ Models/Login.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Assignment2.Models
 {
     public class Login
     {
-        [Required, Range(1000, 9999)]
+        [Key, Required, Range(1, 9999)]
+        [ForeignKey("Customer")]
         public int CustomerID { get; set; }
         public virtual Customer Customer { get; set; }
 
-        [Key, Required, StringLength(50)]
+        [Required, StringLength(50)]
         [Display(Name = "User ID")]
         public string UserID { get; set; }
 
