a9926dd0a037c5141b99445ffb9a85509e23e0ea
Jiahui Li <s3614541@student.rmit.edu.au>
2020-01-29 14:12:37 +1100

    AccountType dropdown list

---
+++ ViewModels/ViewMyStatementVM.cs
@@ -0,0 +1,23 @@
+﻿using Assignment2.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using X.PagedList;
+
+namespace Assignment2.ViewModels
+{
+    public class ViewMyStatementVM
+    {
+        public string SelectedAccountType { get; set; }
+        public IEnumerable<SelectListItem> AccountTypesSelectList { get; set; } =
+            new SelectListItem[]
+                {
+                     new SelectListItem() { Text = "Saving", Value = "Saving" },
+                     new SelectListItem() { Text = "Checking", Value = "Checking" }
+                };
+        public IPagedList<Transaction> PagedListTransactions { get; set; }
+    }
+}
