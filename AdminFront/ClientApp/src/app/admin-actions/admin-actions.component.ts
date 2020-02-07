8fb4802dab3ff73b20d0d4175e8c677ac1c183a6
atirudom <atirach.int@gmail.com>
2020-02-07 16:00:53 +1100

    add account actions

--- AdminFront/ClientApp/src/app/admin-actions/admin-actions.component.ts
+++ AdminFront/ClientApp/src/app/admin-actions/admin-actions.component.ts
@@ -10,12 +10,15 @@ import * as $ from "jquery";
 export class AdminActionsComponent {
   public url = { edit: '', back: '' };
   public customer: Customer;
+  public accounts: Account[];
+  public billPays: BillPay[];
   public customerID: string;
 
   constructor(http: HttpClient, private route: ActivatedRoute) {
     this.customerID = this.route.snapshot.paramMap.get('customerID');
     http.get<Customer>("http://localhost:63637/" + `api/customers/${this.customerID}`).subscribe(result => {
       this.customer = result;
+      this.accounts = this.customer.accounts
       this.url.edit = `/admin-actions/${this.customerID}/edit`
       this.url.back = '/all-users'
       console.log()
@@ -25,36 +28,87 @@ export class AdminActionsComponent {
 
   ngOnInit() { }
 
+  viewBill(accNumber) {
+    $.ajax("http://localhost:63637/" + "api/BillPays/FromAccount/" + accNumber, {
+      success: (data) => {
+        console.log(data);
+        this.billPays = data
+      }
+    })
+  }
+
   lockUser() {
     $.ajax("http://localhost:63637/" + "api/logins/lock/" + this.customerID, {
       type: 'POST',
       success: (data) => {
         console.log(data);
-        this.customer.Login.Status = 'Locked'
+        this.customer.login.status = 'Locked'
       }
     })
   }
 
-  onClickViewCustomer(data) {
-    //this._router.navigate(["/view-user"])
+  blockBillPay(billPayID) {
+    console.log(billPayID);
+
+    $.ajax("http://localhost:63637/" + "api/BillPays/block/" + billPayID, {
+      type: 'PUT',
+      success: (data) => {
+        console.log(data);
+
+        // Does not trigger update
+        //this.billPays.forEach(bill => {
+        //  if (bill.billPayID == billPayID) bill.status = 'Normal'
+        //})
+      }
+    })
+  }
+
+  unblockBillPay(billPayID) {
+    console.log(billPayID);
+    $.ajax("http://localhost:63637/" + "api/BillPays/unblock/" + billPayID, {
+      type: 'PUT',
+      success: (data) => {
+        console.log(data);
+
+        //this.billPays.forEach(bill => {
+        //  if (bill.billPayID == billPayID) bill.status = 'Blocked'
+        //})
+      }
+    })
   }
 }
 
 interface Login {
-  CustomerID: number;
-  UserID: string;
-  LockUntilTime: Date;
-  Status: string;
+  customerID: number;
+  userID: string;
+  lockUntilTime: Date;
+  status: string;
 }
 
 interface Customer {
-  CustomerID: number;
-  Name: string;
-  TFN: string;
-  Address: string;
-  City: string;
-  State: string;
-  PostCode: string;
-  Phone: string;
-  Login: Login;
+  customerID: number;
+  name: string;
+  tfn: string;
+  address: string;
+  city: string;
+  state: string;
+  postCode: string;
+  phone: string;
+  login: Login;
+  accounts: Account[];
+}
+
+interface Account {
+  accountNumber: number;
+  accountType: string;
+  customerID: number;
+  balance: number;
+}
+
+interface BillPay {
+  billPayID: number;
+  accountNumber: number;
+  amount: number;
+  scheduleDate: Date;
+  status: string;
 }
