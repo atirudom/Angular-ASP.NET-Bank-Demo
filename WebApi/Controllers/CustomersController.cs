0555fc925ed6a67b12bb8577539317f95ba5295b
Jiahui Li <s3614541@student.rmit.edu.au>
2020-02-07 15:55:46 +1100

    develop for part c

--- WebApi/Controllers/CustomersController.cs
+++ WebApi/Controllers/CustomersController.cs
@@ -7,6 +7,7 @@ using AdminApi.Models.DataManager;
 using AdminApi.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdminApi.Controllers
 {
@@ -27,12 +28,40 @@ namespace AdminApi.Controllers
         {
             IEnumerable<Customer> customerLists = _repo.GetAll();
             List<CustomerDto> customerDtoLists = new List<CustomerDto>();
-            foreach(var customer in customerLists)
+            foreach (var customer in customerLists)
             {
-                customerDtoLists.Add(new CustomerDto(customer));
+                CustomerDto cusDto = new CustomerDto(customer, _repo.GetLogin(customer.CustomerID));
+                customerDtoLists.Add(cusDto);
             }
 
             return customerDtoLists;
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{customerID}")]
+        public CustomerDto Get(int customerID)
+        {
+            var customer = _repo.Get(customerID);
+            CustomerDto cusDto = new CustomerDto(customer, _repo.GetLogin(customer.CustomerID));
+            cusDto.PopulateAccounts();
+
+            return cusDto;
+        }
+
+        // PUT: api/Customers/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public object Put(int id, CustomerDto cusDto)
+        {
+            Customer customer = cusDto.GenerateModel();
+            if (id != customer.CustomerID)
+            {
+                return BadRequest();
+            }
+            _repo.Update(id, customer);
+
+            return new { success = true };
         }
     }
 }
