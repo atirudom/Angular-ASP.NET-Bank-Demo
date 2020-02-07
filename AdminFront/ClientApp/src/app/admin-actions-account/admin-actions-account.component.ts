574b791946a44f3a6a3d26d54d28a1b9aa2da4e8
atirudom <atirach.int@gmail.com>
2020-02-07 16:00:37 +1100

    prepare page for account admin actions

---
+++ AdminFront/ClientApp/src/app/admin-actions-account/admin-actions-account.component.ts
@@ -0,0 +1,65 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as $ from "jquery";
+
+@Component({
+  selector: 'app-admin-actions-account',
+  templateUrl: './admin-actions-account.component.html'
+})
+export class ActionsAccountComponent {
+  public url = { edit: '', back: '' };
+  public customer: Customer;
+  public accounts: Account[];
+  public customerID: string;
+
+  constructor(http: HttpClient, private route: ActivatedRoute) {
+    this.customerID = this.route.snapshot.paramMap.get('customerID');
+    http.get<Customer>("http://localhost:63637/" + `api/customers/${this.customerID}`).subscribe(result => {
+      this.customer = result;
+      this.accounts = this.customer.accounts
+      this.url.edit = `/admin-actions/${this.customerID}/edit`
+      this.url.back = '/all-users'
+      console.log(this.customer)
+    }, error => console.error(error));
+  }
+
+  ngOnInit() { }
+
+  lockUser() {
+    $.ajax("http://localhost:63637/" + "api/logins/lock/" + this.customerID, {
+      type: 'POST',
+      success: (data) => {
+        console.log(data);
+        this.customer.login.status = 'Locked'
+      }
+    })
+  }
+}
+
+interface Login {
+  customerID: number;
+  userID: string;
+  lockUntilTime: Date;
+  status: string;
+}
+
+interface Customer {
+  customerID: number;
+  name: string;
+  tfn: string;
+  address: string;
+  city: string;
+  state: string;
+  postCode: string;
+  phone: string;
+  login: Login;
+  accounts: Account[];
+}
+
+interface Account {
+  accountNumber: number;
+  accountType: string;
+  customerID: number;
+  balance: number;
+}
