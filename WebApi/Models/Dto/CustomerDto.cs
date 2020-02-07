93b19c5cd8c7e0169e810cc73441a55e30799856
Jiahui Li <s3614541@student.rmit.edu.au>
2020-02-07 15:54:55 +1100

    Add Dto models

--- WebApi/Models/Dto/CustomerDto.cs
+++ WebApi/Models/Dto/CustomerDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,10 +16,18 @@ namespace AdminApi.Models.Dto
         public string State { get; set; }
         public string PostCode { get; set; }
         public string Phone { get; set; }
+        public LoginDto Login { get; set; }
+        public List<AccountDto> Accounts { get; set; }
+
+        [JsonIgnore]
+        public Customer Customer;
 
         public CustomerDto() { }
 
-        public CustomerDto(Customer cus) {
+        public CustomerDto(Customer cus) : this(cus, null) { }
+
+        public CustomerDto(Customer cus, Login login)
+        {
             CustomerID = cus.CustomerID;
             Name = cus.Name;
             TFN = cus.TFN;
@@ -27,6 +36,36 @@ namespace AdminApi.Models.Dto
             State = cus.State.ToString();
             PostCode = cus.PostCode;
             Phone = cus.Phone;
+            Login = login != null ? new LoginDto(login) : null;
+
+            Customer = cus;
+        }
+
+        internal void PopulateAccounts()
+        {
+            Accounts = new List<AccountDto>();
+            var accountList = Customer.Accounts.ToList();
+
+            foreach (var acc in accountList)
+            {
+                Accounts.Add(new AccountDto(acc));
+            };
+        }
+
+        internal Customer GenerateModel()
+        {
+            Customer = new Customer()
+            {
+                CustomerID = CustomerID,
+                Name = Name,
+                TFN = TFN,
+                Address = Address,
+                City = City,
+                State = (AustralianState)Enum.Parse(typeof(AustralianState), State),
+                PostCode = PostCode,
+                Phone = Phone,
+            };
+            return Customer;
         }
     }
 }
