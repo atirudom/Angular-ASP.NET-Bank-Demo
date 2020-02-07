7db782655f3d8242bd87c0c1b13ac25e59f736ff
atirudom <atirach.int@gmail.com>
2020-02-07 16:01:37 +1100

    Add api for bill pays

---
+++ WebApi/Models/DataManager/BillPayManager.cs
@@ -0,0 +1,58 @@
+﻿using AdminApi.Data;
+using AdminApi.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminApi.Models.DataManager
+{
+    public class BillPayManager : IDataRepository<BillPay, int>
+    {
+        private readonly MainContext _context;
+
+        public BillPayManager(MainContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<BillPay> GetFromAccount(int accountNumber)
+        {
+            return _context.Accounts.Find(accountNumber).BillPays;
+        }
+
+        public int Add(BillPay billPay)
+        {
+            _context.BillPays.Add(billPay);
+            _context.SaveChanges();
+
+            return billPay.BillPayID;
+        }
+
+        public int Delete(int billPayID)
+        {
+            _context.BillPays.Remove(_context.BillPays.Find(billPayID));
+            _context.SaveChanges();
+
+            return billPayID;
+        }
+
+        public BillPay Get(int billPayID)
+        {
+            return _context.BillPays.Find(billPayID);
+        }
+
+        public IEnumerable<BillPay> GetAll()
+        {
+            return _context.BillPays.ToList();
+        }
+
+        public int Update(int id, BillPay billPay)
+        {
+            _context.Update(billPay);
+            _context.SaveChanges();
+
+            return id;
+        }
+    }
+}
---
+++ WebApi/Models/Dto/BillPayDto.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminApi.Models.Dto
+{
+    public class BillPayDto
+    {
+        public int BillPayID { get; set; }
+        public int AccountNumber { get; set; }
+        public int PayeeID { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ScheduleDate { get; set; }
+        public string Period { get; set; }
+        public string Status { get; set; }
+        public string StatusMessage { get; set; }
+        public DateTime LastPaymentTime { get; set; }
+        public DateTime ModifyDate { get; set; }
+
+        [JsonIgnore]
+        public BillPay BillPay;
+
+        public BillPayDto(BillPay bill)
+        {
+            BillPayID = bill.BillPayID;
+            AccountNumber = bill.AccountNumber;
+            PayeeID = bill.PayeeID;
+            Amount = bill.Amount;
+            ScheduleDate = bill.ScheduleDate;
+            Period = bill.Period.ToString();
+            Status = bill.Status.ToString();
+            StatusMessage = bill.StatusMessage;
+            LastPaymentTime = bill.LastPaymentTime;
+            ModifyDate = bill.ModifyDate;
+            BillPay = bill;
+        }
+    }
+}
