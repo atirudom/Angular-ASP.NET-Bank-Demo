05ac62a4dfd7c463c23a2d1f2e405964799ad6e9
Jiahui Li <s3614541@student.rmit.edu.au>
2020-02-07 15:58:18 +1100

    develop and finalize for part c.4

--- AdminFront/ClientApp/src/app/app.module.ts
+++ AdminFront/ClientApp/src/app/app.module.ts
@@ -1,34 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AllUsersComponent } from './all-users/all-users.component';
+import { AdminActionsComponent } from './admin-actions/admin-actions.component';
+import { AdminEditCusComponent } from './admin-editcus/admin-editcus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    AllUsersComponent
+    AllUsersComponent,
+    AdminActionsComponent,
+    AdminEditCusComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'all-users', component: AllUsersComponent }
+      { path: 'all-users', component: AllUsersComponent },
+      { path: 'admin-actions/:customerID', component: AdminActionsComponent },
+      { path: 'admin-actions/:customerID/edit', component: AdminEditCusComponent }
     ])
   ],
   providers: [],
