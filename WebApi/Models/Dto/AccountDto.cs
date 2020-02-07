35ec558fa2a9c296a297bd2bddbb7594a2f53b67
Jiahui Li <s3614541@student.rmit.edu.au>
2020-02-07 15:54:55 +1100

    Add Dto models

---
+++ WebApi/Models/Dto/AccountDto.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminApi.Models.Dto
+{
+    public class AccountDto
+    {
+        public int AccountNumber { get; set; }
+        public string AccountType { get; set; }
+        public int CustomerID { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime ModifyDate { get; set; }
+
+        [JsonIgnore]
+        public Account Account;
+
+        public AccountDto(Account acc)
+        {
+            AccountNumber = acc.AccountNumber;
+            AccountType = acc.AccountType.ToString();
+            CustomerID = acc.CustomerID;
+            Balance = acc.Balance;
+            ModifyDate = acc.ModifyDate;
+
+            Account = acc;
+        }
+    }
+}
