88cf58ca7a5813ecdf1c27664f7145b4e171f9c4
Jiahui Li <s3614541@student.rmit.edu.au>
2020-02-07 19:47:44 +1100

    add styleUrls for home

--- AdminFront/ClientApp/src/app/home/home.component.ts
+++ AdminFront/ClientApp/src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { environment } from '../../environments/environment'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls:['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
