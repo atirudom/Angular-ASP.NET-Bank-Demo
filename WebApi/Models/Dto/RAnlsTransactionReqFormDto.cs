23e3e0ef1aa41d393b7a14df9188fff9dbd9e921
atirudom <atirach.int@gmail.com>
2020-02-08 22:36:22 +1100

    Develop Amount per transaction type on API

--- WebApi/Models/Dto/RAnlsTransactionReqFormDto.cs
+++ WebApi/Models/Dto/RAnlsTransactionReqFormDto.cs
@@ -8,6 +8,7 @@ namespace AdminApi.Models.Dto
     // Receiving Analysis Transaction Request Form DTO
     public class RAnsTransactionReqFormDto
     {
+        public int? CustomerID { get; set; }
         public DateTime FromDate { get; set; }
         public DateTime ToDate { get; set; }
         public string ChartType { get; set; }
