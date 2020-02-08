56781ab14d92e8459dcb4c2b08d5ef7d66cae3cd
atirudom <atirach.int@gmail.com>
2020-02-08 11:34:43 +1100

    Add new transaction model and repo

---
+++ WebApi/Models/Dto/RAnlsTransactionReqFormDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminApi.Models.Dto
+{
+    // Receiving Analysis Transaction Request Form DTO
+    public class RAnsTransactionReqFormDto
+    {
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public string ChartType { get; set; }
+    }
+}
