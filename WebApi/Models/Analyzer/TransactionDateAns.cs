482f431a103ed732e6b5a06ed438ef47e40b73af
atirudom <atirach.int@gmail.com>
2020-02-08 11:34:59 +1100

    Add analyzation on API

---
+++ WebApi/Models/Analyzer/TransactionDateAns.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminApi.Models.Analyzer
+{
+    public class TransactionDateAns
+    {
+        public string Date { get; set; }
+        public int TransactionCount { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
